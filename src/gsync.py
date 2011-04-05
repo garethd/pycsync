93551995775ede9a32661cd8e446fad23a9f7e38
garethd <gareth.doutch@gmail.com>
2011-04-05 22:30:19 +0100

    refactoring global params out commonolising album downloads

--- src/gsync.py
+++ src/gsync.py
@@ -14,7 +14,7 @@ TODO: Script improvements
 Limit downloads by:
 * Only get images with highest rating
 * Start with the latest uploads and work backwards
-* Be able to pick which albums to sync
+* Be able to pick which albums to GetUserAlbums
 ** devise some kind of scheme to do this!!!
 
 Limit cache dir to certain size
@@ -29,6 +29,11 @@ TODO: DB indexing
 
 TODO: Other feeds
 Commonlise the GetFeatured one! - how to resize them!!!???
+
+
+FIXME:  GetUserALbums - replace the excusions hack with a proper album selection method
+    # use the include/exclude if statement with passed in lists for now
+    # when we are storing include lists then remove the exclude altogether
 
 '''
 
@@ -46,15 +51,25 @@ APPNAME = 'pycsync'
 
 # FIXME: find system way of storing / retreiving credentials and replace this crap!
 class Logins:
-    username = 'joe.mangle'
+    username = 'madge.bishop'
     email = username + '@gmail.com'
-    password = 'ramseystreetrocks'
+    password = 'neeramsey'
 
 
-gd_client = gdata.photos.service.PhotosService()
+def DoClientLogin(myClient):
+    """The parameters here will determine what gets downloaded
+    if password is not given then only public albums will get downloaded
+    """
+    myClient.username = Logins.username
+    myClient.email = Logins.email
+    myClient.source = APPNAME
+    myClient.password = Logins.password
+    myClient.ProgrammaticLogin()
+    del myClient.password
+    return myClient
 
 
-def GetFeatured(imgmax=None, limit=None):
+def GetFeatured(myClient, imgmax=None, limit=None):
     """Load the first 100 found and 
     Delete old ones"""
     # TODO: resizing this lot
@@ -63,85 +78,54 @@ def GetFeatured(imgmax=None, limit=None):
     current_dir = InitSyncDir(album_dir)
 
 #    if not imgmax:
-    photos = gd_client.GetFeed(uri)
+    photos = myClient.GetFeed(uri)
 #    else:
-#        photos = gd_client.GetFeed("%s&imgmax=%s" % (album.GetPhotosUri(), imgmax))
+#        photos = myClient.GetFeed("%s&imgmax=%s" % (album.GetPhotosUri(), imgmax))
 
     new_files = DownloadPhotos(photos, album_dir, limit=limit)
     RemoveFiles(current_dir - set(new_files), album_dir)
 
 
-def InitSyncDir(album_dir):
-    """ensure the dir exists
-    create if not
-    return the contents list"""
-    if not os.path.exists(album_dir):
-        os.makedirs(album_dir)
-    return set(os.listdir(album_dir))
-
-
-def FormatFileName(file_name):
-    fn, ext = os.path.splitext(file_name)
-    if len(fn) < 2: return ''
-    replace = [' ', ';', ':', '"', "'", '!', '?', ',', '.',
-               '#', '%', '$', r'/', '\\',
-               'http', 'www', 'com']
-    for r in replace: fn = fn.replace(r, '')
-    if len(fn) > 36: fn = fn[:36]
-    return fn + ext
-
-
-def DoClientLogin():
-    """The parameters here will determine what gets downloaded
-    if password is not given then only public albums will get downloaded
-    """
-    gd_client.email = Logins.email
-    gd_client.password = Logins.password
-    gd_client.source = APPNAME
-    gd_client.ProgrammaticLogin()
-
-
-def sync(username, limit=None, imgmax=None,
-         exclusions=['Fireworks', 'Blog', 'Tech']):
-    #FIXME: replace the excusions hack with a proper album selection method
-    prefix = GetCacheDir(username.text)
-    try:
-        album_list = gd_client.GetUserFeed(user=username, limit=limit)
-        for album in reversed(album_list.entry):
-            include = True
-            for exclusion in exclusions:
-                if exclusion in album.title.text: include = False
-            if include:
-                sync_album(album, prefix, imgmax)
-    except socket.error:
-        print 'interwebs is currently down, go make a panad'
-
-
-
-def GetFriendFeeds(username, imgmax=None, limit=None):
+def GetFriendAlbums(myClient, limit=None, imgmax=None,
+                   include=[], exclude=[]):
     """Load the first 100 found and 
     Delete old ones"""
-    for friend in gd_client.GetContacts(username).entry:
+    for friend in myClient.GetContacts(myClient.username).entry:
         prefix = GetCacheDir(friend.title.text)
         uri = friend.GetAlbumsUri()
-        album_list = gd_client.GetFeed(uri)
-        for album in album_list.entry:
-            sync_album(album, prefix, imgmax)
+        album_list = myClient.GetFeed(uri)
+        GetUserAlbums(myClient, album_list, prefix, imgmax, include, exclude, limit=limit)
+#        for album in album_list.entry:
+#            sync_album(album, prefix, imgmax)
 
 
-def GetAlbumDir(album, prefix):
-    if not os.path.isdir(prefix):
-        print "Creating directory %s" % (prefix)
-        os.makedirs(prefix)
-    album_dir = os.path.join(prefix, "%d" % (album.timestamp.datetime().year),
-                             "%d-%02d-%02d %s" % (album.timestamp.datetime().year,
-                                                  album.timestamp.datetime().month,
-                                                  album.timestamp.datetime().day,
-                                                  album.title.text))
-    return album_dir.replace(' ', '_')
+def GetMyAlbums(myClient, limit=None, imgmax=None,
+                include=[], exclude=[]):
+    prefix = GetCacheDir(myClient.username)
+    album_list = myClient.GetUserFeed(user=myClient.username)
+    GetUserAlbums(myClient, album_list, prefix, imgmax, include, exclude, limit)
 
 
-def sync_album(album, prefix, imgmax, limit=None):
+def GetUserAlbums(myClient, album_list, prefix, imgmax=None,
+                  include=[], exclude=[], limit=None):
+    #FIXME: replace the excusions hack with a proper album selection method
+    # use the include/exclude if statement with passed in lists for now
+    # when we are storing include lists then remove the exclude altogether
+    for album in reversed(album_list.entry):
+        syncme = True
+        if include:
+            for inclusion in include:
+                if inclusion in album.title.text: break   # syncme is True anyway
+        elif exclude:
+            for exclusion in exclude:
+                if exclusion in album.title.text:
+                    syncme = False
+                    break
+        if syncme:
+            sync_album(myClient, album, prefix, imgmax, limit=limit)
+
+
+def sync_album(myClient, album, prefix, imgmax, limit=None):
     #TODO: add limit in here
     """
     download the album photos to the dir under prefix
@@ -153,17 +137,11 @@ def sync_album(album, prefix, imgmax, limit=None):
     InitSyncDir(album_dir)
 
     if not imgmax:
-        photos = gd_client.GetFeed(album.GetPhotosUri())
+        photos = myClient.GetFeed(album.GetPhotosUri())
     else:
-        photos = gd_client.GetFeed("%s&imgmax=%s" % (album.GetPhotosUri(), imgmax))
+        photos = myClient.GetFeed("%s&imgmax=%s" % (album.GetPhotosUri(), imgmax))
 
-    DownloadPhotos(photos, album_dir)
-
-
-def RemoveFiles(files, album_dir):
-    for name in files:
-        print "Deleting unused file %s" % (os.path.join(album_dir, name))
-        os.remove(os.path.join(album_dir, name))
+    DownloadPhotos(photos, album_dir, limit)
 
 
 def DownloadPhotos(photos, album_dir, limit=None):
@@ -183,37 +161,92 @@ def DownloadPhotos(photos, album_dir, limit=None):
     return new_files
 
 
+def GetAlbumDir(album, prefix):
+    if not os.path.isdir(prefix):
+        print "Creating directory %s" % (prefix)
+        os.makedirs(prefix)
+    album_dir = os.path.join(prefix, "%d" % (album.timestamp.datetime().year),
+                             "%d-%02d-%02d %s" % (album.timestamp.datetime().year,
+                                                  album.timestamp.datetime().month,
+                                                  album.timestamp.datetime().day,
+                                                  album.title.text))
+    return album_dir.replace(' ', '_')
+
+
+def FormatFileName(file_name):
+    fn, ext = os.path.splitext(file_name)
+    if len(fn) < 2: return ''
+    replace = [' ', ';', ':', '"', "'", '!', '?', ',', '.',
+               '#', '%', '$', r'/', '\\', '(', ')'
+               'http', 'www', 'com']
+    for r in replace: fn = fn.replace(r, '')
+    if len(fn) > 36: fn = fn[:36]
+    return fn + ext
+
+
 def download(url, destination):
+    """save a photo to the file dest"""
     out = open(destination, 'w')
     out.write(urllib2.urlopen(url).read())
 
 
+def InitSyncDir(album_dir):
+    """ensure the dir exists
+    create if not
+    return the contents list"""
+    if not os.path.exists(album_dir):
+        os.makedirs(album_dir)
+    return set(os.listdir(album_dir))
+
+
+def RemoveFiles(files, album_dir):
+    for name in files:
+        print "Deleting unused file %s" % (os.path.join(album_dir, name))
+        os.remove(os.path.join(album_dir, name))
+
+
 def GetCacheDir(username):
     """where to store the photos"""
-#    TODO: mangle the username
-    homedir = os.path.expanduser('~')
+#    TODO: mangle the username?
+    homedir = GetBaseDir()
     return os.path.join(homedir, 'pwebc', username)
 
 
+def GetBaseDir():
+    """Return the dir the user specified as a top level save location
+    default to users home dir"""
+    defaultLocation = os.path.expanduser('~')
+    # TODO: storing and retrieving customised base directory
+    return defaultLocation
+
+
 def main(own=True, friends=True, featured=False):
-    DoClientLogin()
+    myClient = gdata.photos.service.PhotosService()
+    try:
+        myClient = DoClientLogin(myClient)
+    except socket.error:
+        print 'interwebs is currently down, go make a panad'
+        return
+
     if own:
-        Thread(target=sync,
-        args=(Logins.username, GetCacheDir(Logins.username)),
-        kwargs=dict(imgmax='800u')
+        Thread(target=GetMyAlbums,
+        args=(myClient, GetCacheDir(myClient.username)),
+        kwargs=dict(imgmax='800u',
+                    exclude=['Fireworks', 'Blog', 'Tech'])
         ).start()
 
     if friends:
-        Thread(target=GetFriendFeeds,
-               args=(Logins.username,),
-               kwargs=dict(imgmax='800u', limit=100)
+        Thread(target=GetFriendAlbums,
+               args=(myClient,),
+               kwargs=dict(imgmax='800u', limit=10)
         ).start()
 
     if featured:
         Thread(target=GetFeatured,
-        kwargs=dict(imgmax='800u', limit=100)
+               args=(myClient,),
+        kwargs=dict(imgmax='800u', limit=10)
         ).start()
 
 
 if __name__ == '__main__':
-    main(True, False, False)
+    main(True, True, True)
